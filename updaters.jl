f5cf518921227ecfdfc765d7c4667e7a33e0702e
Guanxiong Qu <quguanxiong@gmail.com>
2024-12-27 23:27:48 +0900

    Update updaters.jl

--- updaters.jl
+++ updaters.jl
@@ -2,19 +2,19 @@ using Compat: @compat
 using ITensors: ITensor, array, inds, itensor
 using ITensorTDVP: TimeDependentSum, to_vec
 using KrylovKit: exponentiate
-using OrdinaryDiffEqTsit5: ODEProblem, Tsit5, solve
+#using OrdinaryDiffEqTsit5: ODEProblem, Tsit5, solve
 
-function ode_updater(operator, init; internal_kwargs, alg=Tsit5(), kwargs...)
-  @compat (; current_time, time_step) = (; current_time=zero(Bool), internal_kwargs...)
-  time_span = typeof(time_step).((current_time, current_time + time_step))
-  init_vec, to_itensor = to_vec(init)
-  f(init::ITensor, p, t) = operator(t)(init)
-  f(init_vec::Vector, p, t) = to_vec(f(to_itensor(init_vec), p, t))[1]
-  prob = ODEProblem(f, init_vec, time_span)
-  sol = solve(prob, alg; kwargs...)
-  state_vec = sol.u[end]
-  return to_itensor(state_vec), (;)
-end
+# function ode_updater(operator, init; internal_kwargs, alg=Tsit5(), kwargs...)
+#   @compat (; current_time, time_step) = (; current_time=zero(Bool), internal_kwargs...)
+#   time_span = typeof(time_step).((current_time, current_time + time_step))
+#   init_vec, to_itensor = to_vec(init)
+#   f(init::ITensor, p, t) = operator(t)(init)
+#   f(init_vec::Vector, p, t) = to_vec(f(to_itensor(init_vec), p, t))[1]
+#   prob = ODEProblem(f, init_vec, time_span)
+#   sol = solve(prob, alg; kwargs...)
+#   state_vec = sol.u[end]
+#   return to_itensor(state_vec), (;)
+# end
 
 function krylov_updater(operator, init; internal_kwargs, kwargs...)
   @compat (; current_time, time_step) = (; current_time=zero(Bool), internal_kwargs...)
